373d70ced4c558a23b5c6ac8e2b7c9e206f87765
Jusuf Saiti <jusuf_saiti@drivosity.com>
2022-07-23 13:23:24 +0200

    Initial Commit

---
+++ Comics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Comics
+//
+//  Created by Jusuf Saiti on 23.7.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
